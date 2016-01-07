e84224d8286d7847caf1d03cc1e19e375f148255
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-01-07 13:38:34 +0100

    Init

---
+++ tests/IntegrationTagTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of php-cache\memcache-adapter package.
+ *
+ * (c) 2015-2015 Aaron Scherer <aequasi@gmail.com>, Tobias Nyholm <tobias.nyholm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Cache\Adapter\Memcache\Tests;
+
+use Cache\Adapter\Memcache\MemcacheCachePool;
+use Cache\IntegrationTests\TaggableCachePoolTest;
+
+class IntegrationTagTest extends TaggableCachePoolTest
+{
+    public function createCachePool()
+    {
+        return new MemcacheCachePool();
+    }
+}
